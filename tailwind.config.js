ea759f1178aa15c4af90521fb828b7d529691a3f
Tonny Francis <tonnyfrancis161@poli.ufrj.br>
2023-03-05 20:10:50 -0300

    INIT

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Roboto': ['Roboto'],
+      },
+    },
+    colors: {
+      primary: "#202442",
+      secondary: "#41CD7D"
+    }
+  },
+  plugins: [],
+}
